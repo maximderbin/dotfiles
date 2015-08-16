cd39cc089714eb71184d59c6a4c25a6fa98f4a1f
Maxim Derbin <maxim@derbin.io>
2015-08-16 15:47:09 +0300

    (.bundle-config) Add bundle config

--- tools/install.sh
+++ tools/install.sh
@@ -46,6 +46,15 @@ fi
 echo "\033[0;34mSymlinking .gemrc\033[0m"
 ln -s ~/.dotfiles/.gemrc ~/.gemrc
 
+echo "\033[0;34mLooking for an existing bundler config...\033[0m"
+if [ -f ~/.bundle/config ] || [ -h ~/.bundle/config ]
+then
+  echo "\033[0;33mFound ~/.bundle/config.\033[0m \033[0;32mBacking up to ~/.bundle/config.old\033[0m";
+  mv ~/.bundle/config ~/.bundle/config.old;
+fi
+echo "\033[0;34mSymlinking .gemrc\033[0m"
+ln -s ~/.dotfiles/.bundle-config ~/.bundle/config
+
 echo "\033[0;34mLooking for an existing zsh config...\033[0m"
 if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]
 then
