6522b32569081ac81f226563be76e234eee2fe20
Maxim Derbin <maxim@derbin.io>
2017-04-03 20:29:27 -0700

    (brew.sh) Add brew.sh initial

--- Brewfile
+++
@@ -1,6 +0,0 @@
-install zsh
-install tmux
-install vim --override-system-vi
-install ack
-install ctags-exuberant
-install nginx
---
+++ brew.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+
+brew install git
+brew install zsh
+brew install vim --with-override-system-vi
+brew install ctags-exuberant
+
+brew cask install iterm2
+
+brew cask install skype
