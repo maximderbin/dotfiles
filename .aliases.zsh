7ecef402adf8061cd4958e8f8b9188d9d9584709
Maxim Derbin <maxim@derbin.io>
2017-05-12 12:20:15 -0700

    (.aliases.zsh) Add alias to remove all git branches except master and current

--- .aliases.zsh
+++ .aliases.zsh
@@ -4,3 +4,7 @@ alias tlf="tail -f"
 # Apps
 alias skype="sudo /Applications/Skype.app/Contents/MacOS/Skype"
 alias wthr="curl http://wttr.in/redwood_city"
+
+# Git
+# Remove all branches except master and current
+alias gbda='git branch | egrep -v "(master|\*)" | xargs git branch -D'
