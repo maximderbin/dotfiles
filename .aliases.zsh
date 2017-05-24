9fbafbe12cfb7212e6e1ddeb361ae1a437b70a54
Maxim Derbin <maxim@derbin.io>
2017-05-24 12:30:15 -0700

    (.aliases.zsh) Add git stats alias

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,10 +1,12 @@
 # Unix
-alias tlf="tail -f"
+alias tlf='tail -f'
 
 # Apps
-alias skype="sudo /Applications/Skype.app/Contents/MacOS/Skype"
-alias wthr="curl http://wttr.in/redwood_city"
+alias skype='sudo /Applications/Skype.app/Contents/MacOS/Skype'
+alias wthr='curl http://wttr.in/redwood_city'
 
 # Git
 # Remove all branches except master and current
 alias gbda='git branch | egrep -v "(master|\*)" | xargs git branch -D'
+# Show my stats
+alias gstats='git log --author="Maxim Derbin" --pretty=tformat: --numstat | awk '\''{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s\n", add, subs, loc }'\'''
