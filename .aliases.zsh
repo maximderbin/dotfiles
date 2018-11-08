74cae6a98b7dcccd51837614df6fa9516fbd60ec
Maxim Derbin <maxim@derbin.io>
2018-11-08 11:50:48 -0800

    (.aliasses.zsh) Add release notes shortcut

--- .aliases.zsh
+++ .aliases.zsh
@@ -10,3 +10,7 @@ alias wthr='curl http://wttr.in/redwood_city'
 alias gbda='git branch | egrep -v "(master|\*)" | xargs git branch -D'
 # Show my stats
 alias gstats='git log --author="Maxim Derbin" --pretty=tformat: --numstat | awk '\''{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s\n", add, subs, loc }'\'''
+
+glt() {
+  git log $1..HEAD --no-merges --pretty=format:"%C(yellow)%h %Cblue%>(12)%ad %Cgreen%<(7)%aN%Cred%d %Creset%s"
+}
