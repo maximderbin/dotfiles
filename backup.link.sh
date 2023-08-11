9d4c47b212fb88359cebbcba41976ea537ad56fb
Maxim Derbin <maxim@derbin.io>
2023-08-10 22:31:49 -0700

    (backups) Link .bash_history

--- backup.link.sh
+++ backup.link.sh
@@ -1,3 +1,4 @@
 ln -s /Users/mderbin/Library/CloudStorage/OneDrive-Microsoft/backups/code /Users/mderbin/code
 ln -s /Users/mderbin/Library/CloudStorage/OneDrive-Microsoft/backups/home/.linkedin.env.zsh /Users/mderbin/.linkedin.env.zsh
 ln -sf /Users/mderbin/Library/CloudStorage/OneDrive-Microsoft/backups/home/.zsh_history /Users/mderbin/.zsh_history
+ln -sf /Users/mderbin/Library/CloudStorage/OneDrive-Microsoft/backups/home/.bash_history /Users/mderbin/.bash_history
